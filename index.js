e72d873ccc8d16307253ebf8b202464a32304b80
David Dinh <dmdinh22@gmail.com>
2019-04-03 21:44:38 -0700

    createStore obj with getState method

--- index.js
+++ index.js
@@ -7,3 +7,18 @@
 // Pass store to react-redux's <Provider /> with React app
 
 // Implementation
+
+// store - obj with methods on it
+function createStore() {
+    // create empty state obj on instantiation
+    let state = {};
+
+    // returning the store -> objg with getState func
+    // - calling getState() grants access to state var
+    //   inside createStore closure
+    return {
+        getState() {
+            return state;
+        }
+    };
+}
