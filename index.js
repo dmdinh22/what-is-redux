73d7b45242ed2a58d95cbab78d22d2e463b5a54f
David Dinh <dmdinh22@gmail.com>
2019-04-03 21:37:06 -0700

    comments about redux

---
+++ index.js
@@ -0,0 +1,9 @@
+// ## REDUX ##
+// core part of redux is the store
+// Store has a single state tree
+// - reads state
+// - dispatch actions to update staet
+// - subscribe & unsubscribe for updates to that state (watchers)
+// Pass store to react-redux's <Provider /> with React app
+
+// Implementation
